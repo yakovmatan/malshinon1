4965f9f252500d4659beed068e253c2fcfa5bf6e
yakov matan <yakovmatan@gmail.com>
2025-06-10 20:51:49 +0300

    create manager

--- manager/Manager.cs
+++ manager/Manager.cs
@@ -10,23 +10,43 @@ namespace malshinon1.manager
     internal class Manager
     {
         private HelpManeger Helper;
+        private Dal Dal;
 
         public Manager(Dal dal)
         {
             Helper = new HelpManeger(dal);
+            Dal = dal;
         }
 
         public void StartUsing()
         {
-            (string firstNameReporter, string lastNameReporter) = Helper.EnterName();
+            (string firstNameReporter, string lastNameReporter) = Helper.EnterName();//  Request a name from the user
             if (!this.Helper.ExistsInTheSystem(firstNameReporter, lastNameReporter))
             {
                 this.Helper.CreateNewPerson(firstNameReporter, lastNameReporter,"reporter");
             }
-            else
+            var reporter = this.Dal.GetPersonByName(firstNameReporter, lastNameReporter);
+            if (reporter.type == "target")
             {
-                this.Helper.UpdateStatusForTarget(firstNameReporter, lastNameReporter);
+                this.Dal.UpdateStatusToBoth(firstNameReporter, lastNameReporter);
             }
+            this.Dal.UpdateReportCount(reporter.secretCode);// Update the reporter on the number of reports
+            string report = this.Helper.EnterReport();// Report request
+            (string firstNameOfTarget, string lastNameOfTarget) = Helper.ExtractName(report);
+            if (!this.Helper.ExistsInTheSystem(firstNameOfTarget, lastNameOfTarget))
+            {
+                this.Helper.CreateNewPerson(firstNameOfTarget, lastNameOfTarget, "target");
+            }
+            var person = this.Dal.GetPersonByName(firstNameReporter, lastNameReporter);
+            if (reporter.type == "target")
+            {
+                this.Dal.UpdateStatusToBoth(firstNameReporter, lastNameReporter);
+            }
+
+
+
+
+
 
         }
     }
