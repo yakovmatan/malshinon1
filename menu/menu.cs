62eb8917d09a975353cf85930c746544f7f5b2d7
yakov matan <yakovmatan@gmail.com>
2025-06-11 15:01:25 +0300

    menu

---
+++ menu/menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using malshinon1.dal;
+using malshinon1.manager;
+
+namespace malshinon1.menu
+{
+    internal class menu
+    {
+        private Dal dal = new Dal();
+        private Manager manager;
+
+    }
+}
